e18e30a42cfcf8a809b49954a92f4be458c46a70
kyledls <kad387@scarletmail.rutgers.edu>
2025-10-03 06:56:04 -0400

    zz

--- day_9/task.py
+++ day_9/task.py
@@ -1,5 +1,26 @@
-programming_dictionary = {
-    "Bug": "An error in a program that prevents the program from running as expected.",
-    "Function": "A piece of code that you can easily call over and over again."
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
 }
 
+student_grades = {}
+
+for name in student_scores:
+    Grade = ""
+    if student_scores[name] >= 91 and student_scores[name] <= 100:
+        Grade = "Outstanding"
+        student_grades.update({name : Grade})
+    elif student_scores[name] >= 81 and student_scores[name] <= 90:
+        Grade = "Exceed Expectations"
+        student_grades.update({name : Grade})
+    elif student_scores[name] >= 71 and student_scores[name] <= 80:
+        Grade = "Acceptable"
+        student_grades.update({name : Grade})
+    else:
+        Grade = "Fail"
+        student_grades.update({name : Grade})
+
+print(student_grades)
---
+++ day_9/task2.py
@@ -0,0 +1,15 @@
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin"
+}
+
+travel_log = {
+    "France": {
+        "num_times_visited": 8,
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+    }
+}
+
+
+print(travel_log["France"]["cities_visited"][2])
+
